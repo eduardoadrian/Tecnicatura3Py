a98340aaa4b2795350746ad2595078f8a6d6fbf3
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-06-01 20:55:13 -0300

    Leccion04 clase4,5 registros con Postgresql y Python

--- Leccion04/BD/prueba_bd.py
+++ Leccion04/BD/prueba_bd.py
@@ -1,18 +1,17 @@
-import psycopg2 # esto es para poder conectarnos a Postgre
+import psycopg2 # Esto es para poder conectar a Postgre
 
-conexion = psycopg2.connect(
-    user='postgres',
-    password='admin',
-    host='127.0.0.1',
-    port='5432',
-    database='test_bd'
-)
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_BD')
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #Placeholder
+            id_persona = input('Digite un número para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,)) # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchone() # Recuperamos todos los registros que seran una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrio un error: {e}')
+finally:
+    conexion.close()
 
-cursor =  conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
-registros = cursor.fetchall() # de esta manera recuperamos todos los registros que seran una lista
-print(registros)
-
-cursor.close()
-conexion.close()
+# https://www.psycopg.org/docs/usage.html

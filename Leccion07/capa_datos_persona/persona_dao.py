ff64d8b60f03b3a4def816d01a98893a9a9b4534
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-08-13 21:12:48 -0300

    clase 11 Pool de conexiones en Python y Postgresql

--- Leccion07/capa_datos_persona/persona_dao.py
+++ Leccion07/capa_datos_persona/persona_dao.py
@@ -1,5 +1,6 @@
 from capa_datos_persona.Persona import Persona
 from capa_datos_persona.conexion import Conexion
+from capa_datos_persona.cursor_del_pool import CursorDelPool
 from logger_base import log
 
 
@@ -22,59 +23,55 @@ class PersonaDAO:
 
       @classmethod
        def seleccionar(cls):
-            with Conexion.obtenerConexion():
-                with Conexion.obtnerCursor() as cursor:
-                    cursor.execute(cls._SELECCIONAR)
-                    registros = cursor.fetchall()
-                    personas = [] # creamos una lista
-                    for registro in registros:
-                        persona = Persona(registro[0], registro[1], registro[2], registro[3])
-                        personas.append(persona)
-                    return personas
+           with CursorDelPool as cursor:
+               cursor.execute(cls._SELECCIONAR)
+               registros = cursor.fetchall()
+               personas = [] # creamos una lista
+               for registro in registros:
+                   persona = Persona(registro[0], registro[1], registro[2], registro[3])
+                   personas.append(persona)
+               return personas
 
        @classmethod
        def insertar(cls, persona):
-            with Conexion.obtenerConexion():
-                with Conexion.obtenerCursor() as cursor:
-                    valores = (persona.nombre, persona.apellido, persona.email)
-                    cursor.execute(cls._INSERTAR, valores)
-                    log.debug(f'Persona Insertada: {persona}')
-                    return cursor.rowcount
+           with CursorDelPool() as cursor:
+                valores = (persona.nombre, persona.apellido, persona.email)
+                cursor.execute(cls._INSERTAR, valores)
+                log.debug(f'Persona Insertada: {persona}')
+                return cursor.rowcount
 
        @classmethod
-       def actualiar(cls, persona):
-            with Conexion.obtenerConexion():
-                with Conexion.obtnerCursor() as cursor:
-                    valores = (persona.nombre, persona.apellido.email.id_persona)
-                    cursor.execute(cls._ACTUALIZAR, valores)
-                    log.debug(f'Persona actualizada: {persona}')
-                    return cursor.rowcount
+       def actualizar(cls, persona):
+            with CursorDelPool() as cursor:
+                valores = (persona.nombre, persona.apellido.email.id_persona)
+                cursor.execute(cls._ACTUALIZAR, valores)
+                log.debug(f'Persona actualizada: {persona}')
+                return cursor.rowcount
 
        @classmethod
        def eliminar(cls, persona):
-            with Conexion.obtenerConexion():
-                with Conexion.obtenerCursos() as cursor:
-                     valores = (persona.id_persona,)
-                     cursor.execute(cls._ELIMINAR, valores)
-                     log.debug(f'los objetos eliminados son: {persona}')
-                     return cursor.rowcount
+            with CursorDelPool() as cursor:
+                 valores = (persona.id_persona,)
+                 cursor.execute(cls._ELIMINAR, valores)
+                 log.debug(f'los objetos eliminados son: {persona}')
+                 return cursor.rowcount
 
 
 if __name__ == '__main__':
     # Eliminar un registro
-    # persona1 = Persona(id_persona=8)
-    # personas_eliminadas = PersonaDAO.eliminar(persona1)
-    # log.debug(f'Personas eliminadas: {personas:eliminadas}')
+    persona1 = Persona(id_persona=18)
+    personas_eliminadas = PersonaDAO.eliminar(persona1)
+    log.debug(f'Personas eliminadas: {personas:eliminadas}')
 
     # Actualizar un registro
-    # persona1 = Persona(1, 'Juan Jose', 'Pena', 'jjpena@mail.com')
-    # personas_actualizadas = PersonaDAO.actualizar(persona1)
-    # log.debug(f'Personas actualizadas: {personas_actualizadas}')
+    persona1 = Persona(1, 'Juan', 'Pena', 'jpena@mail.com')
+    personas_actualizadas = PersonaDAO.actualizar(persona1)
+    log.debug(f'Personas actualizadas: {personas_actualizadas}')
 
     # Insertar un registro
-    # persona1 = Persona(nombre='Omero', apellido='Ramos', email='omeror@mail.com')
-    # personas_insertadas = PersonaDAO.insertar(persona1)
-    # log.debug(f'Personas insertadas: {personas_insertadas}')
+    persona1 = Persona(nombre='Mateo', apellido='Torres', email='tmateo@mail.com')
+    personas_insertadas = PersonaDAO.insertar(persona1)
+    log.debug(f'Personas insertadas: {personas_insertadas}')
 
 
     # Seleccionar objetos
